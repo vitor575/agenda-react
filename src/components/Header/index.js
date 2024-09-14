c3fb7e2413225a512fdd5c0bef08f60739d77eea
Vitor Hugo <vitor95340@gmail.com>
2024-09-14 01:36:58 -0300

    Inicio do projeto agenda

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <header className="cabecalho">
+            <h1>AGENDA</h1>
+        </header>
+    )
+};
+
+export default Header;
